d5f93847f906884e0435454875a3d1b56b34fa84
erufenix <erufenix@gmail.com>
2020-01-21 23:25:39 -0600

    Crear usuario

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -4,14 +4,29 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class UserFixtures extends Fixture
-{
+use App\Entity\User;
+
+class UserFixtures extends Fixture{
+
+    public function __construct(UserPasswordEncoderInterface $encoder){
+        $this->encoder = $encoder;
+    }
+
+
     public function load(ObjectManager $manager)
     {
         // $product = new Product();
         // $manager->persist($product);
+        $user = new User();
 
+        $password = $this->encoder->encodePassword($user, 'qwerty');
+        $user->setPassword($password);                     
+        $user->setEmail('erufenix@gmail.com');
+        $user->setRoles(['ROLE_ADMIN']);
+        
+        $manager->persist($user);
         $manager->flush();
     }
 }
